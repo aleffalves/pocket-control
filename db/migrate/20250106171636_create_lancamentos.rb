aecb4e785139063619346a5572d02a5d77e97545
aleff-alves <aleff.alves@tangerino.com.br>
2025-01-08 21:53:14 -0300

    ajuste migration

--- db/migrate/20250106171636_create_lancamentos.rb
+++ db/migrate/20250106171636_create_lancamentos.rb
@@ -1,7 +1,7 @@
 class CreateLancamentos < ActiveRecord::Migration[7.1]
   def change
     create_table :lancamentos do |t|
-      t.references :consumidores, null: false, foreign_key: true
+      t.references :consumidors, null: false, foreign_key: true
       t.references :categoria, null: false, foreign_key: true
       t.decimal :valor
       t.datetime :data
--- db/migrate/20250106182619_alter_referencia_consumidores_para_consumidors.rb
+++
@@ -1,7 +0,0 @@
-class AlterReferenciaConsumidoresParaConsumidors < ActiveRecord::Migration[7.1]
-  def change
-        remove_reference :lancamentos, :consumidor, foreign_key: true
-    
-        add_reference :lancamentos, :consumidors, null: false, foreign_key: true
-  end
-end
