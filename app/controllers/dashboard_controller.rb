f75b07873d1338d55e0998f940bef61e8636a8f3
aleff-alves <aleff.alves@tangerino.com.br>
2025-01-26 12:29:49 -0300

    adicionando grafico anual

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -28,9 +28,33 @@ class DashboardController < ApplicationController
   end
 
   def mensal
+    @lancamentos_por_semana_mes = Lancamento.where(data: Date.today.beginning_of_month..Date.today.end_of_month)
+    .group_by { |l| l.data.beginning_of_week(:sunday) }
+    .transform_values { |lancamentos| lancamentos.sum(&:valor_cents) }
+
+    @lancamentos_categoria_mes = Lancamento.joins(:categorium)
+    .where(data: Date.today.beginning_of_month..Date.today.end_of_month)
+    .group('categoria.nome')
+    .sum(:valor_cents)
+
+    @lancamentos_valor_mes = Lancamento.where(data: Date.today.beginning_of_month..Date.today.end_of_month).sum(:valor_cents) / 100.0
+    @lancamentos_valor_semanal = Lancamento.where(data: Date.today.beginning_of_week..Date.today.end_of_week).sum(:valor_cents) / 100.0
   end
 
   def anual
+    @lancamentos_por_mes_ano = Lancamento.where(data: Date.today.beginning_of_year..Date.today.end_of_year)
+    .group_by { |l| l.data.beginning_of_month }
+    .transform_values { |lancamentos| lancamentos.sum(&:valor_cents) }
+
+    @lancamentos_categoria_ano = Lancamento.joins(:categorium)
+    .where(data: Date.today.beginning_of_year..Date.today.end_of_year)
+    .group('categoria.nome')
+    .sum(:valor_cents)
+
+    @lancamentos_valor_ano = Lancamento.where(data: Date.today.beginning_of_year..Date.today.end_of_year).sum(:valor_cents) / 100.0
+    @lancamentos_valor_mes = Lancamento.where(data: Date.today.beginning_of_month..Date.today.end_of_month).sum(:valor_cents) / 100.0
+
+    Rails.logger.debug @lancamentos_por_mes_ano
   end
 
 end
